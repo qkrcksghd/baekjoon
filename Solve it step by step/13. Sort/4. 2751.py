feef4ca2cb55ee6893b6d3660bbde73beeca6724
qkrcksghd <157493719+qkrcksghd@users.noreply.github.com>
2024-02-17 20:50:20 +0900

    Update 4. 2751.py

--- Solve it step by step/13. Sort/4. 2751.py
+++ Solve it step by step/13. Sort/4. 2751.py
@@ -1,2 +1,14 @@
 # 수 정렬하기 2
 
+import sys
+input = sys.stdin.readline
+
+array = []
+
+for _ in range(int(input())):
+    array.append(int(input()))
+
+array.sort()
+
+for i in range(len(array)):
+    print(array[i])
